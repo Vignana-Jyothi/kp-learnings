d7107d8a62fabe45213b2dc1a8436d3a49b01175
KP <head.iie@vnrvjiet.in>
2024-06-28 05:46:57 +0530

    Improve Logging #9

--- gen-ai/exp1/chat.py
+++ gen-ai/exp1/chat.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI, Request
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import torch
-
+import logging
+import time
 
 app = FastAPI()
 
 model_path = "./gpt2-finetuned"
 model = GPT2LMHeadModel.from_pretrained(model_path)
 tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+logging.basicConfig(level=logging.INFO)
 
 
 @app.post("/chat")
 async def chat(request: Request):
+    start_time = time.time()
     data = await request.json()
     question = data.get("question")
     inputs = tokenizer.encode(question, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=500, num_return_sequences=1)
+    
+    outputs = model.generate(inputs, max_length=500, num_return_sequences=1, temperature=0.7, top_k=1)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return {"answer": response}
-
-# Run the app
-# uvicorn script_name:app --reload
+    
+    end_time = time.time()
+    logging.info(f"Response time: {end_time - start_time:.2f} seconds")
+    
+    return {"My Answer": response}
